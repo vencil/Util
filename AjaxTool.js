dc7bc67d51c64152674afa5543502675a9feab83
vencil <vencsvencil@gmail.com>
2013-11-02 19:48:56 +0800

    Update AjaxTool.js

--- AjaxTool.js
+++ AjaxTool.js
@@ -61,3 +61,59 @@ function ajaxPost(callback, url, sync, postQueryString) {
 
     return true;
 }
+
+
+/*
+**Cross Domain Use
+*/
+
+//for GET request
+function crossDomainGet(url, postata, callback) {
+    var head = document.getElementsByTagName('head')[0];
+    var js = document.createElement('script');
+
+    if (url.indexOf('?') === -1) url += '?' + postata;
+    else url += '&' + postata;
+    if (callback) url += '&callback=' + callback;
+    js.setAttribute('src', url);
+    if (head) {
+        head.appendChild(js);
+    } else {
+        document.body.appendChild(js);
+    }
+}
+
+//for POST request
+function crossDomainPost(url, postData) {
+    // Add the iframe with a unique name
+    var iframe = document.createElement("iframe");
+    document.body.appendChild(iframe);
+    iframe.style.display = "none";
+    iframe.contentWindow.name = "postIframe";
+
+    // construct a form with hidden inputs, targeting the iframe
+    var form = document.createElement("form");
+    form.target = "postIframe";
+    form.action = url;
+    form.method = "POST";
+
+    // repeat for each parameter
+    var data = postData.split('&');
+    for (var i = 0; i < data.length; i++) {
+        var input = document.createElement("input");
+        var _d = data[i];
+        input.type = "hidden";
+        input.name = _d.slice(0, _d.indexOf('='));
+        input.value = _d.slice(_d.indexOf('=') + 1);
+        form.appendChild(input);
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+
+    //remove after post
+    document.body.removeChild(form);
+    iframe.onload = function () {
+        document.body.removeChild(iframe);
+    };
+}
